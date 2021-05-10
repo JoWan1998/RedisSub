5c5b4ed777a7c89904d238cc2427eb3456ae3b27
Jose Wannan <2993999310101@ingenieria.usac.edu.gt>
2021-05-10 01:55:42 -0600

    cambios prueba

--- room.go
+++ room.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"context"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
@@ -71,30 +70,12 @@ func subscribeMessages() {
 		if err != nil {
 			log.Fatal("Post nuevo documento... ", err)
 		}
-		defer req.Body.Close() // cerramos el body
-
-		//Leyendo la respuesta del cuerpo
-		nuevo, err := ioutil.ReadAll(req.Body) //se convierte en cadena
-		if err != nil {
-			log.Fatal("Leyendo Respuesta desde el Post Http... ", err)
-		}
-		sb := string(nuevo) //lo transformamos en una cadena
-		log.Printf(sb)
 		//http://35.223.156.4:7019/nuevoRegistro
 		req1, err1 := http.Post("http://35.223.156.4:7019/nuevoRegistro", "application/json", bytes.NewBuffer(post)) //hacemos la peticion a la bd
 		req1.Header.Set("Content-Type", "application/json")
 		if err1 != nil {
 			log.Fatal("Post nuevo documento... ", err1)
 		}
-		defer req1.Body.Close() // cerramos el body
-
-		//Leyendo la respuesta del cuerpo
-		nuevo1, err1 := ioutil.ReadAll(req1.Body) //se convierte en cadena
-		if err1 != nil {
-			log.Fatal("Leyendo Respuesta desde el Post Http... ", err)
-		}
-		sb1 := string(nuevo1) //lo transformamos en una cadena
-		log.Printf(sb1)
 	}
 }
 
